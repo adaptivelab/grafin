b2191eded5804f1b695a730511ee92be4d203203
James Gorrie <james.gorrie@adaptivelab.co.uk>
2013-03-18 18:16:18 +0000

    Basic data handling features

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,24 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jasmine: {
-      src: 'src/**/*.js',
-      options: {
-        specs: 'spec/**/*.js'
+      grafin: {
+        src: 'src/**/*.js',
+        options: {
+          host: 'http://127.0.0.1:8000',
+          vendor: [
+            'components/d3/d3.js',
+            'src/d3.chart/chart.js',
+            'src/d3.chart/stacked-bar.js',
+            'data/data.js',
+            'data/modelData.js'
+          ],
+          specs: 'spec/**/*.js',
+          outfile: 'test.html'
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-
   grunt.registerTask('test', ['jasmine']);
 } 
--- spec/grafin.js
+++ spec/grafin.js
@@ -1,6 +1,31 @@
 'use strict';
 
-xdescribe("Grafin", function() {
-  xit ('Should return a bargraph for D3 useage');
-  xit ('Should take in SoMA data and return the right data for D3 chart layout');
+describe('Grafin', function() {
+  it ('Should return a stacked bargraph for D3 useage', function() {
+    var chart = d3.chart.stackedBar;
+    expect((typeof chart).toLowerCase()).toBe('function');
+  });
+
+  it ('Should take in SoMA data and return the right data for D3 chart layout', function() {
+    var modelStack = d3.layout.stack()
+      , layerCount = data.labels.length
+      , svg = d3.select('body').append('svg')
+      , stack = d3.layout.stack()
+          .values(function(d) {
+            console.log(d);
+            return d;
+          });
+
+    // prep model data
+    modelData = d3.range(layerCount).map(function() { return modelData });
+    
+    // TODO: prep data
+    
+
+
+    // svg.selectAll('rect').data();
+    // svg.selectAll('rect').data(stack(data));
+
+  });
 });
+
