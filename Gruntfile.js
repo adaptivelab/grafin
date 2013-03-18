a4e14f60b11864b3c857f05f0a3c8092c9deb188
James Gorrie <james.gorrie@adaptivelab.co.uk>
2013-03-18 12:39:52 +0000

    Added Jasmine as the test library

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    jasmine: {
+      src: 'src/**/*.js',
+      options: {
+        specs: 'spec/**/*.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('test', ['jasmine']);
+} 
